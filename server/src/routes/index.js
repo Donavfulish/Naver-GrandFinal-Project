24dbddaaddd15735d2c76df48fa16431d9fc3b5f
thisYui <160146960+thisYui@users.noreply.github.com>
2025-11-17 18:10:01 +0700

    DELETE Spotify

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,7 +3,6 @@ import authRoutes from './auth.routes.js';
 import userRoutes from './user.routes.js';
 import spaceRoutes from './space.routes.js';
 import searchRoutes from './search.routes.js';
-import spotifyRoutes from './spotify.routes.js';
 
 const router = express.Router();
 
@@ -12,6 +11,4 @@ router.use('/auth', authRoutes);       // /api/auth/*
 router.use('/users', userRoutes);      // /api/users/*
 router.use('/spaces', spaceRoutes);     // /api/spaces/*
 router.use('/search', searchRoutes);    // /api/search/*
-router.use('/spotify', spotifyRoutes);  // /api/spotify/*
-
 export default router;
